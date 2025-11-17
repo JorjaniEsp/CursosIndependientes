9bf22b9cdcd9217bfb9e75146664049f1a01bb78
JorjaniEsp <espinozajorjanie2006@gmail.com>
2025-11-17 16:50:43 -0600

    feat: segunda clase del curso java master, ejemplos de variables

--- SectionVariables/src/HelloWord.java
+++ SectionVariables/src/HelloWord.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+
 public class HelloWord {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        String saludar = "Hello world";
+
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = 15;
+
 
     }
 
